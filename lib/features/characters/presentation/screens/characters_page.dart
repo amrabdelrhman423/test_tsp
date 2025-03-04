b107c810b7eab907c5b7d6c13f8c928fb7f7e763
amrabdelrhman423 <amrmedo432@gmail.com>
2025-03-04 14:40:43 +0200

    add logo .

--- lib/features/characters/presentation/screens/characters_page.dart
+++ lib/features/characters/presentation/screens/characters_page.dart
@@ -20,37 +20,84 @@ class CharacterListPage extends StatelessWidget {
     }
   }
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Rick and Morty Characters')),
-      body: BlocBuilder<CharacterCubit, CharacterState>(
-        bloc: _characterCubit,
-        builder: (context, state) {
-          if (state is CharacterLoading && _characterCubit.characters.isEmpty) {
-            return Center(child: CircularProgressIndicator());
-          } else if (state is CharacterLoaded) {
-            return ListView.builder(
-              controller: _scrollController,
-              itemCount: state.characters.length + 1,
-              itemBuilder: (context, index) {
-                if (index == state.characters.length) {
-                  return Center(child: CircularProgressIndicator());
+      body: Column(
+        children: [
+          // _buildSearchBar(),
+          Expanded(
+            child: BlocBuilder<CharacterCubit, CharacterState>(
+              bloc: _characterCubit,
+              builder: (context, state) {
+                if (state is CharacterLoading && _characterCubit.characters.isEmpty) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (state is CharacterLoaded) {
+                  return ListView.builder(
+                    controller: _scrollController,
+                    itemCount: state.characters.length + 1,
+                    itemBuilder: (context, index) {
+                      if (index == state.characters.length) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                      final character = state.characters[index];
+                      return ListTile(
+                        leading: Image.network(character.image),
+                        title: Text(character.name),
+                        subtitle: Text('${character.status} - ${character.species}'),
+                      );
+                    },
+                  );
+                } else if (state is CharacterError) {
+                  return Center(child: Text(state.message));
                 }
-                final character = state.characters[index];
-                return ListTile(
-                  leading: Image.network(character.image),
-                  title: Text(character.name),
-                  subtitle: Text(character.status),
-                );
+                return const Center(child: Text('No Characters'));
               },
-            );
-          } else if (state is CharacterError) {
-            return Center(child: Text(state.message));
-          }
-          return Center(child: Text('No data'));
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  // Widget _buildSearchBar() {
+  //   return Padding(
+  //     padding: const EdgeInsets.all(8.0),
+  //     child: Row(
+  //       children: [
+  //         Expanded(
+  //           child: TextField(
+  //             onChanged: (value) {
+  //               _characterCubit.fetchCharacters(name: value);
+  //             },
+  //             decoration: InputDecoration(
+  //               labelText: 'Search by name',
+  //               border: OutlineInputBorder(),
+  //             ),
+  //           ),
+  //         ),
+  //         SizedBox(width: 8),
+  //         DropdownButton<String>(
+  //           hint: Text('Status'),
+  //           onChanged: (value) {
+  //             _characterCubit.fetchCharacters(status: value ?? '');
+  //           },
+  //           items: ['Alive', 'Dead', 'unknown']
+  //               .map((status) => DropdownMenuItem(value: status, child: Text(status)))
+  //               .toList(),
+  //         ),
+  //         SizedBox(width: 8),
+  //         DropdownButton<String>(
+  //           hint: Text('Species'),
+  //           onChanged: (value) {
+  //             _characterCubit.fetchCharacters(species: value ?? '');
+  //           },
+  //           items: ['Human', 'Alien']
+  //               .map((species) => DropdownMenuItem(value: species, child: Text(species)))
+  //               .toList(),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 }
